d79cc76becb55297ecc861171fba4bd8d4647c10
Dante <dantenavaza2005@gmail.com>
2025-01-16 20:02:33 -0300

    Update settings.py

--- ecommerce_main/ecommerce/settings.py
+++ ecommerce_main/ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('secret_key')
 IP_ADDRESS = os.getenv('ip_address')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [IP_ADDRESS, '127.0.0.1', 'localhost']
 
