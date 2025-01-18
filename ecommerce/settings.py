dfbf8a2ca68176578d36040927644896b18809f3
Dante <dantenavaza2005@gmail.com>
2025-01-18 12:04:52 -0300

    test

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('secret_key')
 IP_ADDRESS = os.getenv('ip_address')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [IP_ADDRESS, '127.0.0.1', 'localhost', 'reserva-ecommerce-django-production.up.railway.app', 'reserva-ecommerce.com']
 
